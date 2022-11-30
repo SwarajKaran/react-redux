972b2a9e9360688f29f859be54eeead94d4db30c
SwarajKaran <swarajkaran.raj@gmail.com>
2022-11-30 18:04:04 +0530

    useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const incrementHandler = () => {
+    dispatch({ type: 'INCR' });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'DECR' });
+  };
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action === 'INCR') {
+  if (action.type === 'INCR') {
     return {
       counter: state.counter + 1,
     };
   }
-  if (action === 'DECR') {
+  if (action.type === 'DECR') {
     return {
       counter: state.counter - 1,
     };
