c5919cdcc1ea1afb46b9774d1f19409fb306b012
SwarajKaran <swarajkaran.raj@gmail.com>
2022-12-01 00:15:18 +0530

    Split store index file to multiple splice files

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,11 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-const initialCounterState = { counter: 0, showCounter: true };
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter';
+import authReducer from './auth';
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-const initialAuthState = { isAuthenticated: false };
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+
+export default store;
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === 'INCR') {
 //     return {
@@ -59,12 +34,7 @@ const authSlice = createSlice({
 //   return state;
 // };
 // const store = createStore(counterReducer);
-const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
-});
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
-export default store;
+
 // const counterSubscriber = () => {
 //   const latestState = store.getState();
 //   console.log(latestState);
